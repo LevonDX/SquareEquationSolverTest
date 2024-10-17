d468e1796da28e6bbfcf0a69cb831b2882b74b5e
levondx <clevon85@outlook.com>
2024-10-17 20:43:01 +0400

    usings removed from Complex Num

--- SquareEquationSolverTest/ComplexNum.cs
+++ SquareEquationSolverTest/ComplexNum.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SquareEquationSolverTest
+﻿namespace SquareEquationSolverTest
 {
     public class ComplexNum : IComparable<ComplexNum>
     {
